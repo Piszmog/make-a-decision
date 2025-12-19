d1631bd1bd363e3ed5aa6a71821f43f6161e640d
Randell <piszmogcode@gmail.com>
2025-12-19 12:46:16 -0700

    Clear

--- internal/db/queries/query.sql
+++ internal/db/queries/query.sql
@@ -1,92 +1,111 @@
--- name: GetAuthor :one
-SELECT * FROM authors
-WHERE id = ? LIMIT 1;
-
--- name: ListAuthors :many
-SELECT * FROM authors
-ORDER BY name;
-
--- name: CreateAuthor :one
-INSERT INTO authors (
-  name, bio
-) VALUES (
-  ?, ?
-)
-RETURNING *;
-
--- name: UpdateAuthor :exec
-UPDATE authors
-SET name = ?,
-bio = ?
-WHERE id = ?;
-
--- name: DeleteAuthor :exec
-DELETE FROM authors
-WHERE id = ?;
-
 -- name: GetOptions :many
-SELECT * FROM options
-ORDER BY created_at;
+SELECT
+  *
+FROM
+  options
+ORDER BY
+  created_at;
 
 -- name: GetOption :one
-SELECT * FROM options
-WHERE id = ? LIMIT 1;
+SELECT
+  *
+FROM
+  options
+WHERE
+  id = ?
+LIMIT
+  1;
 
 -- name: CreateOption :one
-INSERT INTO options (
-  name, bio, duration_minutes, weight
-) VALUES (
-  ?, ?, ?, ?
-)
-RETURNING id, name, bio, duration_minutes, weight, created_at;
+INSERT INTO
+  options (name, bio, duration_minutes, weight)
+VALUES
+  (?, ?, ?, ?) RETURNING id,
+  name,
+  bio,
+  duration_minutes,
+  weight,
+  created_at;
 
 -- name: UpdateOption :exec
 UPDATE options
-SET name = ?,
-    bio = ?,
-    duration_minutes = ?,
-    weight = ?
-WHERE id = ?;
+SET
+  name = ?,
+  bio = ?,
+  duration_minutes = ?,
+  weight = ?
+WHERE
+  id = ?;
 
 -- name: UpdateDuration :exec
 UPDATE options
-SET duration_minutes = ?
-WHERE id = ?;
+SET
+  duration_minutes = ?
+WHERE
+  id = ?;
 
 -- name: UpdateWeight :exec
 UPDATE options
-SET weight = ?
-WHERE id = ?;
+SET
+  weight = ?
+WHERE
+  id = ?;
 
 -- name: DeleteOption :exec
 DELETE FROM options
-WHERE id = ?;
+WHERE
+  id = ?;
 
 -- name: GetOrCreateTag :one
-INSERT INTO tags (name) 
-VALUES (LOWER(?))
-ON CONFLICT(name) DO UPDATE SET name = LOWER(excluded.name)
-RETURNING *;
+INSERT INTO
+  tags (name)
+VALUES
+  (LOWER(?)) ON CONFLICT (name) DO
+UPDATE
+SET
+  name = LOWER(excluded.name) RETURNING *;
 
 -- name: GetTagsForOption :many
-SELECT t.id, t.name, t.created_at FROM tags t
-INNER JOIN option_tags ot ON t.id = ot.tag_id
-WHERE ot.option_id = ?
-ORDER BY ot.created_at;
+SELECT
+  t.id,
+  t.name,
+  t.created_at
+FROM
+  tags t
+  INNER JOIN option_tags ot ON t.id = ot.tag_id
+WHERE
+  ot.option_id = ?
+ORDER BY
+  ot.created_at;
 
 -- name: AddTagToOption :exec
-INSERT OR IGNORE INTO option_tags (option_id, tag_id) 
-VALUES (?, ?);
+INSERT
+OR IGNORE INTO option_tags (option_id, tag_id)
+VALUES
+  (?, ?);
 
 -- name: ClearTagsForOption :exec
-DELETE FROM option_tags WHERE option_id = ?;
+DELETE FROM option_tags
+WHERE
+  option_id = ?;
 
 -- name: DeleteUnusedTags :exec
-DELETE FROM tags 
-WHERE id NOT IN (SELECT DISTINCT tag_id FROM option_tags);
+DELETE FROM tags
+WHERE
+  id NOT IN (
+    SELECT DISTINCT
+      tag_id
+    FROM
+      option_tags
+  );
 
 -- name: GetAllTags :many
-SELECT DISTINCT t.id, t.name, t.created_at 
-FROM tags t
-INNER JOIN option_tags ot ON t.id = ot.tag_id
-ORDER BY t.name;
+SELECT DISTINCT
+  t.id,
+  t.name,
+  t.created_at
+FROM
+  tags t
+  INNER JOIN option_tags ot ON t.id = ot.tag_id
+ORDER BY
+  t.name;
